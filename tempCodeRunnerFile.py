194ed9222015ce3cb4036c7c3a0270305c100cb7
Mariam <msedrakyan14@gmmail.com>
2026-07-09 23:30:30 +0400

    dsdcdsc

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,6 @@
-my_list=[15,20,26,260,249,3365,4452,8]
-if my_list[0] > my_list[1]:
-    temp = my_list[0]
-    my_list[0] = my_list[1]
-    my_list[1] = temp
-
-print("sorted", my_list)
+print("sorted", my_list)]
+minimum=my_list[0]
+for number in my_list:
+   if number<minimum:
+      minimum=number
+print("minimum_value", minimum)
